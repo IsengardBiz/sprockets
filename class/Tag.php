9ff148263e2f7ba76085f5b8f7fd7e7da66c6487
Simon <simon@isengard.biz>
2012-08-02 06:25:57 +0000

    Add ->toArrayWithoutOverrides() to reduce queries associated with certain getVar() overrides; remove some debug code.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/sprockets/trunk@24365 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- class/Tag.php
+++ class/Tag.php
@@ -197,6 +197,43 @@ class SprocketsTag extends icms_ipf_seo_Object {
 		return $button;
 	}
 	
+	/*
+	 * Performs the same function as toArray(), but does not permit getVar() overrides for specified
+	 * fields (ie. those requiring query lookups), so that they can be *manually* overriden from 
+	 * buffers. This can substantially reduce the number of queries when converting a large number 
+	 * of objects (for example, on an index page).
+	 */
+	public function toArrayWithoutOverrides() {
+		$ret = $vars = $blacklisted_vars = array();
+		
+		// These are the properties that we don't want converted, because each one costs a query
+		$blacklisted_vars = array('parent_id', 'mid');
+		
+		$vars = $this->getVars();
+		foreach ($vars as $key=>$var) {
+			if (in_array($key, $blacklisted_vars)) {
+				$value = $this->getVar($key, 'e');
+				$ret[$key] = $value;
+			} else {
+				$value = $this->getVar($key);
+				$ret[$key] = $value;
+			}
+		}
+		if ($this->handler->identifierName != "") {
+			$controller = new icms_ipf_Controller($this->handler);
+			/**
+			 * Addition of some automatic value
+			 */
+			$ret['itemLink'] = $controller->getItemLink($this);
+			$ret['itemUrl'] = $controller->getItemLink($this, TRUE);
+			$ret['editItemLink'] = $controller->getEditItemLink($this, FALSE, TRUE);
+			$ret['deleteItemLink'] = $controller->getDeleteItemLink($this, FALSE, TRUE);
+			$ret['printAndMailLink'] = $controller->getPrintAndMailLink($this);
+		}
+
+		return $ret;
+	}
+	
 	/**
 	 * Returns a html snippet containing an RSS icon and link to the feed URL for a given tag
 	 * 
