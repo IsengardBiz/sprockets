d6f99ee34fc65ceb6a59153f0bfb73e11579956e
René Sato <sato-san@impresscms.org>
2012-10-19 07:03:43 +0000

    Sprockets should not visible in the main menu from the front end, since there is no function for the user.
    git-svn-id: https://subversion.assembla.com/svn/impresscmsaddons/modules/sprockets/trunk@24640 7a4d4d67-0c3a-0410-b24c-89d50c169223

--- icms_version.php
+++ icms_version.php
@@ -70,7 +70,7 @@ $modversion['onUpdate'] = "include/onupdate.inc.php";
 $modversion['hasSearch'] = 0;
 
 /** Menu information */
-$modversion['hasMain'] = 1;
+$modversion['hasMain'] = 0;
 
 $modversion['blocks'][1] = array(
   'file' => 'sprockets_content_recent.php',
